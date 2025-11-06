67f29eaff937cbfe140b8b27baae3a09fb6ed9d9
Hugo Cruz <dunkletodeus@gmail.com>
2025-11-06 17:53:42 +0100

    pantalla game + selector pausa + colores + mejoras en game

--- app/src/main/java/com/example/myapplication/Game.kt
+++ app/src/main/java/com/example/myapplication/Game.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class Game : AppCompatActivity() {
@@ -8,5 +10,11 @@ class Game : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        val boton = findViewById<ImageButton>(R.id.butonPausa)
+        val overlay = findViewById<View>(R.id.pauseEfect)
+        boton.setOnClickListener {
+            boton.isSelected = !boton.isSelected
+            overlay.visibility = if (boton.isSelected) View.VISIBLE else View.GONE
+        }
     }
 }
