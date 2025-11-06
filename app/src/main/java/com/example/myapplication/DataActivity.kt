9c74df1e543e609e01a1092688780dd5e331e38a
natt666 <nataaaliaia@gmail.com>
2025-11-06 18:03:52 +0100

    layout start

--- app/src/main/java/com/example/myapplication/DataActivity.kt
+++ app/src/main/java/com/example/myapplication/DataActivity.kt
@@ -3,8 +3,6 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class DataActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/myapplication/StartActivity.kt
+++ app/src/main/java/com/example/myapplication/StartActivity.kt
@@ -1,20 +1,25 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_start)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnPlay = findViewById<ImageButton>(R.id.playButton)
+
+        btnPlay.setOnClickListener {
+            val intent = Intent(this, DataActivity::class.java)
+            startActivity(intent)
         }
+
+
     }
+
 }
