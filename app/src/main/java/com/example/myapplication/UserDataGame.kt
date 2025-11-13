ece4a9746ca499b213af9dcbd6c34cdd7ce03cf5
natt666 <nataaaliaia@gmail.com>
2025-11-13 16:07:18 +0100

    añadido los intent

--- app/src/main/java/com/example/myapplication/UserDataGame.kt
+++ app/src/main/java/com/example/myapplication/UserDataGame.kt
@@ -6,9 +6,9 @@ import java.util.Date
 import java.util.Locale
 
 class UserDataGame(
-    val gameTime: Int? = null,
-    val errors: Int? = null,
-    val points: Int? = null,
-    val date: String = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault()).format(Date()),
+    var gameTime: Int? = null,
+    var errors: Int? = null,
+    var points: Int? = null,
+    var date: String = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault()).format(Date()),
     val dificulty: String
 ) : Serializable
